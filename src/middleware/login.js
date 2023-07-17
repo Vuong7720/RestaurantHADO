0ee48db16d1917ac152b9e88613623ae24b1f87c
pham quoc vuong <vuong7720@gmail.com>
2023-07-18 00:07:23 +0700

    edit midleware login

--- src/middleware/login.js
+++ src/middleware/login.js
@@ -1,20 +1,35 @@
-
+const AccountModel = require('../models/accountModel');
 const {mutipleMongooseToObject} = require('../util/mongoose')
 const jwt = require('jsonwebtoken')
 
 class login {
-    logAdmin(req, res, next){
-        try{
-            var token = req.cookies.token
-            var result = jwt.verify(token,'mk')
-            if(result){
-                next()
-            } 
-        }catch(err){
-            return res.redirect('/login')
+    logAdmin(req, res, next) {
+        try {
+          var token = req.cookies.token;
+          var result = jwt.verify(token, 'mk');
+          if (result) {
+            // Tìm kiếm tài khoản từ AccountModel dựa trên _id trong JWT
+            AccountModel.findById(result._id)
+              .then(account => {
+                if (account) {
+                 next()
+                } else {
+                  return res.redirect('/login');
+                }
+              })
+              .catch(err => {
+                console.log(err);
+                return res.redirect('/login');
+              });
+          } else {
+            return res.redirect('/login');
+          }
+        } catch (err) {
+          console.log(err);
+          return res.redirect('/login');
         }
+      }
     }
-}
-
-module.exports = new login
+    
+    module.exports = new login;
 
